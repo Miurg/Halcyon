8e49563b68de15d9b2d5062014e8cd05bb878477
Miurg <rashnimorgen@gmail.com>
2025-08-17 00:58:31 +0300

    Feat(WIndowSize): Add constructor

--- Src/GLFWCore/Components/WindowSizeComponent.h
+++ Src/GLFWCore/Components/WindowSizeComponent.h
@@ -3,4 +3,5 @@
 struct WindowSizeComponent : Component
 {
 	unsigned int WindowWidth = 0, WindowHeight = 0;
+	WindowSizeComponent(unsigned int width, unsigned int height) : WindowWidth(width), WindowHeight(height) {}
 };
