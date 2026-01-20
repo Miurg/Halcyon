87d4be7c837bf549b2488b94ed0d8fe5316e71e3
Shimu <rashnimorgen@gmail.com>
2026-01-20 11:22:14 +0100

    refactor(shader): small changes in shader

--- src/Graphics/Components/CameraComponent.hpp
+++ src/Graphics/Components/CameraComponent.hpp
@@ -5,7 +5,7 @@ struct CameraComponent
 	float fov = 60.0f;
 	float zNear = 0.1f;
 	float zFar = 1000.0f;
-	float orthoSize = 15.0f;
+	float orthoSize = 5.0f;
 	int bufferNubmer = -1;
 
 	CameraComponent() = default;
