a613f95a0e430fdfe5b36abc754890398e5fc1be
Shimu <rashnimorgen@gmail.com>
2026-01-29 08:22:16 +0100

    refactor(comp): add helpfull method

--- src/Graphics/Components/RelationshipComponent.hpp
+++ src/Graphics/Components/RelationshipComponent.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../Core/Entitys/EntityManager.hpp"
+#include "../../Core/GeneralManager.hpp"
 
 const Entity NULL_ENTITY = 0;
 struct RelationshipComponent
@@ -7,5 +8,24 @@ struct RelationshipComponent
 	Entity parent = NULL_ENTITY;
 	Entity firstChild = NULL_ENTITY;
 	Entity nextSibling = NULL_ENTITY;
-	Entity prevSibling = NULL_ENTITY; 
+	Entity prevSibling = NULL_ENTITY;
+
+	void AddChild(Entity myEntity, Entity childEntity, GeneralManager& gm)
+	{
+		auto* childRel = gm.getComponent<RelationshipComponent>(childEntity);
+		if (childRel == nullptr)
+		{
+			childRel = gm.addComponent<RelationshipComponent>(
+			    childEntity); // If the child entity does not have a RelationshipComponent, add one
+		}
+		childRel->parent = myEntity;
+		childRel->nextSibling = firstChild;
+		childRel->prevSibling = NULL_ENTITY;
+
+		if (firstChild != NULL_ENTITY)
+		{
+			gm.getComponent<RelationshipComponent>(firstChild)->prevSibling = childEntity;
+		}
+		firstChild = childEntity;
+	}
 };
