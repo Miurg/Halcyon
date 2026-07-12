780bd460c269482b4d838087d9cafd467cbc4ff9
Shimu <rashnimorgen@gmail.com>
2026-07-12 20:24:36 +0200

    refactor: remove win32 things

--- src/pch.h
+++ src/pch.h
@@ -2,11 +2,9 @@
 
 #include "ThirdPartyConfig.hpp"
 
-#define VK_USE_PLATFORM_WIN32_KHR
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #define GLFW_INCLUDE_NONE
-#define GLFW_EXPOSE_NATIVE_WIN32
 #include "vk_mem_alloc.h"
 #include <vector>
 #include <memory>
